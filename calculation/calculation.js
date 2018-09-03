492feaa6b75b1154dd60630bdf0275d20a1f7c87
Alex A <alexander.tank@gmx.de>
2018-09-03 22:58:04 +0200

    finished calculation :)

--- calculation/calculation.js
+++ calculation/calculation.js
@@ -1,9 +1,30 @@
 
-function calculate(roofHeight,  houseWidth, houseHeight) {
-  const roofArea = 1/2 * roofHeight * houseWidth;
+document.getElementById("calculateButton").addEventListener("click", function () {
+  const roofHeigt = document.getElementById("roofHeight").value;
+  const houseWidth = document.getElementById("houseWidth").value;
+  const houseHeight = document.getElementById("houseHeight").value;
+
+  if (roofHeigt <= 0) {
+    alert("Roof height needs to be greater than zero.")
+  }
+
+  if (houseWidth <= 0) {
+    alert("House width needs to be greater than zero.")
+  }
+
+  if (houseHeight <= 0) {
+    alert("House height needs to be greater than zero.")
+  }
+
+  const houseArea = calculateHouseArea(roofHeigt, houseWidth, houseHeight);
+  document.getElementById("resultLabel").innerText = "House front area is " + houseArea + "m².";
+});
+
+function calculateHouseArea(roofHeight, houseWidth, houseHeight) {
+  const roofArea = 1 / 2 * roofHeight * houseWidth;
   const houseArea = houseHeight * houseWidth;
 
-  const totalHouseHeight = roofArea + houseArea;
+  const totalHouseArea = roofArea + houseArea;
 
-  return totalHouseHeight;
+  return totalHouseArea;
 }
