43fd8bbf34c195e35ae2a78675182b4d1638ae64
Alex A <alexander.tank@gmx.de>
2018-09-04 19:40:08 +0200

    ref #2

--- calculation/calculation.js
+++ calculation/calculation.js
@@ -1,23 +1,23 @@
 
-document.getElementById("calculateButton").addEventListener("click", function () {
+document.getElementById("calculateButton").addEventListener("click", () => {
   const roofHeigt = document.getElementById("roofHeight").value;
   const houseWidth = document.getElementById("houseWidth").value;
   const houseHeight = document.getElementById("houseHeight").value;
 
   if (roofHeigt <= 0) {
-    alert("Roof height needs to be greater than zero.")
+    alert("Roof height needs to be greater than zero.");
   }
 
   if (houseWidth <= 0) {
-    alert("House width needs to be greater than zero.")
+    alert("House width needs to be greater than zero.");
   }
 
   if (houseHeight <= 0) {
-    alert("House height needs to be greater than zero.")
+    alert("House height needs to be greater than zero.");
   }
 
-  const houseArea = calculateGableArea(roofHeigt, houseWidth, houseHeight);
-  document.getElementById("resultLabel").innerText = "Gable area is " + houseArea + "m².";
+  const gableArea = calculateGableArea(roofHeigt, houseWidth, houseHeight);
+  document.getElementById("resultLabel").innerText = "Gable area is " + gableArea + "m².";
 });
 
 function calculateGableArea(roofHeight, houseWidth, houseHeight) {
