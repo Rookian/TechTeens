4714a1d2619294b8a86139e75900129d519134dc
Alex A <alexander.tank@gmx.de>
2018-09-22 14:25:41 +0200

    figur can move

--- game/game.js
+++ game/game.js
@@ -1,10 +1,44 @@
 let figurImage;
+let vx;
+let vy;
+let x;
+let y;
+
 function setup() {
-  figurImage = loadImage('images/Figur.png');
-  createCanvas(window.screen.width, window.screen.height);
+  figurImage = loadImage('images/Figur2.png');
+  createCanvas(windowWidth, windowHeight);
   background(0);
+  vx = 0;
+  vy = 0;
+  x = 0;
+  y = 0;
 }
 
 function draw() {
-  image(figurImage, 0, 0, 514, 586);
+
+  x = x + vx;
+  y = y + vy;
+
+  if (keyIsDown(LEFT_ARROW)) {
+    vx = -10;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    vx = 10;
+  }
+  else if (keyIsDown(UP_ARROW)) {
+    vy = -10;
+  } else if (keyIsDown(DOWN_ARROW)) {
+    vy = 10;
+  }
+
+  background(0);
+  image(figurImage, x, y, 514, 586);
+}
+
+function keyPressed() {
+
+}
+
+function keyReleased() {
+  vx = 0;
+  vy = 0;
 }
