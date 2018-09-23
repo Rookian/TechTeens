77066aa7d21856ece482cd31d1df63f84cd230f4
Alex A <alexander.tank@gmx.de>
2018-09-23 20:56:24 +0200

    jumping

--- game/game.js
+++ game/game.js
@@ -3,6 +3,7 @@ let vx;
 let vy;
 let x;
 let y;
+const groundY = 400;
 
 function setup() {
   figurImage = loadImage('images/Figur2.png');
@@ -11,7 +12,7 @@ function setup() {
   vx = 0;
   vy = 0;
   x = 0;
-  y = 0;
+  y = groundY;
 }
 
 function draw() {
@@ -19,26 +20,45 @@ function draw() {
   x = x + vx;
   y = y + vy;
 
-  if (keyIsDown(LEFT_ARROW)) {
-    vx = -10;
-  } else if (keyIsDown(RIGHT_ARROW)) {
-    vx = 10;
+  if (y <= 100) {
+    vy += 5;
   }
-  else if (keyIsDown(UP_ARROW)) {
-    vy = -10;
-  } else if (keyIsDown(DOWN_ARROW)) {
-    vy = 10;
+  if (y >= groundY) {
+    vy = 0;
+    if (keyIsDown(UP_ARROW)){
+      this.jump();
+    }
   }
 
   background(0);
-  image(figurImage, x, y, 514, 586);
+  fill(255,0,130);
+  line(0, groundY, windowWidth, groundY);
+  ellipse(150, 150, 100, 100);
+  
+  image(figurImage, x, y);
+  console.log(vy)
 }
 
 function keyPressed() {
+  console.log("keyPressed");
+  if (keyCode === LEFT_ARROW) {
+    vx = -10;
+  } else if (keyCode === RIGHT_ARROW) {
+    vx = 10;
+  }
+  else if (keyCode === UP_ARROW) {
+    this.jump();
+  }
+}
 
+function jump() {
+  if (y === groundY) {
+    vy -= 20;
+  }
 }
 
 function keyReleased() {
-  vx = 0;
-  vy = 0;
+  if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
+    vx = 0;
+  }
 }
