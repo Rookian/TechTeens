9b17ae4286c102fc4621fe09c0282eef38eb3698
Alexander Tank <Alexander.Tank@comparex.de>
2018-10-13 16:08:39 +0200

    fixes image size for ani walk and idle

--- game/game.js
+++ game/game.js
@@ -1,5 +1,8 @@
 const groundY = 400;
 
+//walk height: 197
+// jump height: 198
+
 let figureImage;
 let figure;
 
@@ -17,11 +20,11 @@ function preload() {
 }
 
 function setup() {
-  const walkImages = Sprite.getSpriteImages(15, walkImage, 158, 197);
-  const idleImages = Sprite.getSpriteImages(8, idleImage, 204, 176);
-  const jumpImages = Sprite.getSpriteImages(2, jumpImage, 122, 198)
-  dog = new Sprite(idleImages, walkImages, jumpImages, 0.4, 0, groundY + 112);
-  figure = new Sprite([figureImage], [figureImage], [figureImage], 0, 200, groundY)
+  const walkImages = Sprite.getSpriteImages(15, walkImage, 142, 177);
+  const idleImages = Sprite.getSpriteImages(8, idleImage, 204, 196);
+  const jumpImages = Sprite.getSpriteImages(2, jumpImage, 122, 198);
+  dog = new Sprite(idleImages, walkImages, jumpImages, 0.4, 0, groundY);
+  figure = new Sprite([figureImage], [figureImage], [figureImage], 0, 200, groundY - 100)
 
   createCanvas(windowWidth, windowHeight);
   background(0);
@@ -30,7 +33,7 @@ function setup() {
 function draw() {
   background(0);
   stroke(100, 200, 100);
-  line(0, groundY + figureImage.height, windowWidth, groundY + figureImage.height);
+  line(0, groundY + 200, windowWidth, groundY + 200);
 
   figure.show();
   figure.animate();
