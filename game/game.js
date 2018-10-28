bcf43ea90095178f9aaaa45c978722bd56e9f992
Alex A <alexander.tank@gmx.de>
2018-10-28 20:54:00 +0100

    ref

--- game/game.js
+++ game/game.js
@@ -12,43 +12,43 @@ let idleImage;
 let jumpImage;
 
 function preload() {
-  // Muss in preload stehen
-  figureImage = loadImage('images/Figur2.png');
-  walkImage = loadImage('images/dog/walk.png'); 
-  idleImage = loadImage('images/dog/idle.png');
-  jumpImage = loadImage('images/dog/jump.png');
+	// Muss in preload stehen
+	figureImage = loadImage("images/Figur2.png");
+	walkImage = loadImage("images/dog/walk.png"); 
+	idleImage = loadImage("images/dog/idle.png");
+	jumpImage = loadImage("images/dog/jump.png");
 }
 
 function setup() {
-  const walkImages = Sprite.getSpriteImages(15, walkImage, 142, 177);
-  const idleImages = Sprite.getSpriteImages(8, idleImage, 204, 196);
-  const jumpImages = Sprite.getSpriteImages(2, jumpImage, 122, 198);
-  dog = new Sprite(idleImages, walkImages, jumpImages, 0.4, 0, groundY);
-  figure = new Sprite([figureImage], [figureImage], [figureImage], 0, 200, groundY - 100)
+	const walkImages = Sprite.getSpriteImages(15, walkImage, 142, 177);
+	const idleImages = Sprite.getSpriteImages(8, idleImage, 204, 196);
+	const jumpImages = Sprite.getSpriteImages(2, jumpImage, 122, 198);
+	dog = new Sprite(idleImages, walkImages, jumpImages, 0.4, 0, groundY + 24);
+	figure = new Sprite([figureImage], [figureImage], [figureImage], 0, 200, groundY - 105);
 
-  createCanvas(windowWidth, windowHeight);
-  background(0);
+	createCanvas(windowWidth, windowHeight);
+	background(0);
 }
 
 function draw() {
-  background(0);
-  stroke(100, 200, 100);
-  line(0, groundY + 200, windowWidth, groundY + 200);
+	background(0);
+	stroke(100, 200, 100);
+	line(0, groundY + 200, windowWidth, groundY + 200);
 
-  figure.show();
-  figure.animate();
+	figure.show();
+	figure.animate();
 
-  dog.show();
-  dog.animate();
+	dog.show();
+	dog.animate();
 }
 
 function keyPressed() {
-  dog.keyPressed(keyCode);
-  figure.keyPressed(keyCode);
+	dog.keyPressed(keyCode);
+	figure.keyPressed(keyCode);
 }
 
 function keyReleased() {
-  dog.keyReleased(keyCode);
-  figure.keyReleased(keyCode);
+	dog.keyReleased(keyCode);
+	figure.keyReleased(keyCode);
 }
 
