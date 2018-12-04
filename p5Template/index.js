d9cf4e9299fddc27f973b85a717b102b03f110c9
Alexander Tank <Alexander.Tank@comparex.com>
2018-12-04 18:41:41 +0100

    minimal version for p5

--- p5Template/index.js
+++ p5Template/index.js
@@ -1,36 +1,27 @@
 /// <reference path="./p5.global-mode.d.ts" />
 
-let x = 0;
-let wave;
+// Dokumentation
+// https://p5js.org/
+// https://p5js.org/reference/#group-Shape (2D Primitives)
+// https://p5js.org/examples/
+// https://p5js.org/examples/data-variables.html
+
+// https://github.com/Rookian/TechTeens/blob/master/p5js%20Cheat%20Sheet.md 
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    wave = new p5.TriOsc(0);
-    wave.setType('');
-   
-   
-    wave.start();
 }
 
 function draw() {
-    background(255);
-    wave.freq(x + 200);
-    wave.amp(x/100/3);
-    if (x < 400){
-        x += 1;
-    }   
-    else {
-        wave.stop();
-    }
+    // Schwarzer Hintergrund
+    background(0);
 
-    fill(200,100,100);
-    rect(x, 200, 300, 400, 10, 10, 10, 10);
+    // Rotes Rechteck
+    fill(255, 10, 75);
+    rect(200, 200, 300, 400);
 
-    fill(200,100,200);
-    ellipse(300, 300, x, x / 3  );
-}
-
-function mousePressed(){
-    fill(0,0,0);
-    ellipse(mouseX, mouseY, 50, 50);
-    return false;
+    // Weißer Text
+    fill(255,255,255);
+    textSize(100);
+    text("Guten morgen :)", 300, 300);
 }
